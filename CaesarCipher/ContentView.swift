38b95b472f5d406262d6b4403ac13ed8e3c2931d
mizech <michael.zech@hotmail.de>
2024-01-06 15:16:43 +0100

    Impl.: Algorithm

--- CaesarCipher/ContentView.swift
+++ CaesarCipher/ContentView.swift
@@ -1,12 +1,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var input = ""
+    @State var output = ""
+    @State var shift  = 1
+    let alphabet = ["a", "b", "c", "d", "e", "f",
+                    "g", "h", "i", "j", "k", "l",
+                    "m", "n", "o", "p", "q", "r",
+                    "s", "t", "u", "v", "w", "x",
+                    "y", "z"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Caesar Cipher").font(.title)
+            Form {
+                Section("Text to Encrypt/Decrypt") {
+                    TextField("Text to Encrypt/Decrypt", text: $input)
+                        .lineLimit(2)
+                    Button("Start") {
+                        let lInput = input.lowercased()
+                        output = ""
+                        
+                        for char in lInput {
+                            let index = alphabet.firstIndex(of: String(char))
+                            
+                            if let index = index {
+                                let shiftedIndex = (index + shift) % alphabet.count
+                                output = "\(output)\(alphabet[shiftedIndex])"
+                            } else {
+                                output = "\(output)\(char)"
+                            }
+                        }
+                    }
+                    .padding(.top, 20)
+                    .buttonStyle(.borderedProminent)
+                        .frame(maxWidth: .infinity)
+                }
+                
+                Section("Encrypt/Decrypt Result") {
+                    Text(output).bold()
+                }
+            }
         }
         .padding()
     }
