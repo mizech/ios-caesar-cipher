b83e8970736584719e0dc4e3bead4757f672ab11
mizech <michael.zech@hotmail.de>
2024-01-06 11:51:58 +0100

    Initial Commit

---
+++ CaesarCipher/CaesarCipherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaesarCipherApp.swift
+//  CaesarCipher
+//
+//  Created by Michael on 06.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CaesarCipherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaesarCipher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaesarCipher
+//
+//  Created by Michael on 06.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
