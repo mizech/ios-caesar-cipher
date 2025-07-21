d3ee69c8ddb05dd1d9b1769028b8a215e69dbcc2
mizech <michael.zech@hotmail.de>
2025-07-21 15:53:24 +0200

    Fixed: Form doesn't expand full-size.

--- CaesarCipher/ContentView.swift
+++ CaesarCipher/ContentView.swift
@@ -14,85 +14,85 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Caesar Cipher").font(.title)
-                Form {
-                    Section("Text to Encrypt/Decrypt") {
-                        TextField(
-                            "Insert text here",
-                            text: $input
-                        )
-                        .lineLimit(2)
-                        .disabled(hasSubmitted)
-                        .textInputAutocapitalization(.never)
-                        .autocorrectionDisabled()
-                    }
-                    
-                    Section("Shift") {
-                        Picker("Digits", selection: $shift) {
-                            ForEach(1...25, id: \.self) { digit in
-                                Text("\(digit)")
-                            }
-                        }
-                    }
-                    
-                    Section("Actions") {
-                        VStack(spacing: 20) {
-                            if hasSubmitted {
-                                Button(action: {
-                                    input = ""
-                                    output = ""
-                                    hasSubmitted = false
-                                    shift = 1
-                                }, label: {
-                                    Label("Reset", systemImage: "xmark.circle")
-                                        .foregroundColor(.red)
-                                        .frame(maxWidth: .infinity)
-                                }).buttonStyle(.bordered)
-                                
-                                Button(action: {
-                                    self.clipboard.string = output
-                                }, label: {
-                                    Label("Copy", systemImage: "doc.on.doc")
-                                        .foregroundColor(.green)
-                                        .frame(maxWidth: .infinity)
-                                }).buttonStyle(.bordered)
-                            } else {
-                                Button(action: {
-                                    if let clipboardStr = clipboard.string {
-                                        input = "\(input)\(clipboardStr)"
-                                    }
-                                }, label: {
-                                    Label("Paste", systemImage: "doc.on.clipboard.fill")
-                                        .foregroundColor(.green)
-                                        .frame(maxWidth: .infinity)
-                                }).buttonStyle(.bordered)
-                                CryptButton(
-                                    imageName: "eye.slash",
-                                    caption: LocalizedStringResource("Encrypt", defaultValue: "Encrypt"),
-                                    input: $input,
-                                    output: $output,
-                                    hasSubmitted: $hasSubmitted
-                                ) { index, count in
-                                    (index + shift) % count
-                                }
-                                CryptButton(
-                                    imageName: "eye",
-                                    caption: LocalizedStringResource("Decrypt", defaultValue: "Decrypt"),
-                                    input: $input,
-                                    output: $output,
-                                    hasSubmitted: $hasSubmitted
-                                ) { index, count in
-                                    ((index - shift) + count) % count
-                                }
-                            }
-                        }
-                    }
-                    Section("Encrypt/Decrypt Result") {
-                        Text(output).bold()
-                    }
-                }
-            }
+			Form {
+				Section("Text to Encrypt/Decrypt") {
+					TextField(
+						"Insert text here",
+						text: $input
+					)
+					.lineLimit(2)
+					.disabled(hasSubmitted)
+					.textInputAutocapitalization(.never)
+					.autocorrectionDisabled()
+				}
+				
+				Section("Shift") {
+					Picker("Digits", selection: $shift) {
+						ForEach(1...25, id: \.self) { digit in
+							Text("\(digit)")
+						}
+					}
+				}
+				
+				Section("Actions") {
+					VStack(spacing: 20) {
+						if hasSubmitted {
+							Button(action: {
+								input = ""
+								output = ""
+								hasSubmitted = false
+								shift = 1
+							}, label: {
+								Label("Reset", systemImage: "xmark.circle")
+									.foregroundColor(.red)
+									.frame(maxWidth: .infinity)
+							}).buttonStyle(.bordered)
+							
+							Button(action: {
+								self.clipboard.string = output
+							}, label: {
+								Label("Copy", systemImage: "doc.on.doc")
+									.foregroundColor(.green)
+									.frame(maxWidth: .infinity)
+							}).buttonStyle(.bordered)
+						} else {
+							Button(action: {
+								if let clipboardStr = clipboard.string {
+									input = "\(input)\(clipboardStr)"
+								}
+							}, label: {
+								Label("Paste", systemImage: "doc.on.clipboard.fill")
+									.foregroundColor(.green)
+									.frame(maxWidth: .infinity)
+							}).buttonStyle(.bordered)
+							CryptButton(
+								imageName: "eye.slash",
+								caption: LocalizedStringResource("Encrypt", defaultValue: "Encrypt"),
+								input: $input,
+								output: $output,
+								hasSubmitted: $hasSubmitted
+							) { index, count in
+								(index + shift) % count
+							}
+							CryptButton(
+								imageName: "eye",
+								caption: LocalizedStringResource("Decrypt", defaultValue: "Decrypt"),
+								input: $input,
+								output: $output,
+								hasSubmitted: $hasSubmitted
+							) { index, count in
+								((index - shift) + count) % count
+							}
+						}
+					}
+				}
+				Section("Encrypt/Decrypt Result") {
+					Text(output).bold()
+				}
+			}
+			.navigationTitle("Caesar Cipher")
+			.navigationBarTitleDisplayMode(.inline)
+			.frame(maxWidth: .infinity)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button(action: {
@@ -109,8 +109,7 @@ struct ContentView: View {
                     })
                 }
             }
-            .padding()
-        }
+		}.frame(maxWidth: .infinity)
     }
 }
 
